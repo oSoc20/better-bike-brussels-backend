1dd5a1b251a1dbd782e2555bf0c3c47d365d7d00
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-07-10 10:52:15 +0200

    fix CORS issue

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
 
-const apiRoutes = require("./routes/api")
+const apiRoutes = require("./routes/api");
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Allow-Control-Alow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use("/api", apiRoutes);
 
