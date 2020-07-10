07de230ded2a25d67c6726403f848b7239977ef2
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-07-10 13:31:03 +0200

    add endpoint to get all map endpoints

--- api/v1/routes/map.js
+++ api/v1/routes/map.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const geojson = require('geojson');
-const cache = require('../scripts/cache');
+const geojson = require("geojson");
+const cache = require("../scripts/cache");
 
 router.get("/bicycle-parking", (req, res) => bicycleParking(req, res));
 router.get("/villo-stations", (req, res) => villoStation(req, res));
-router.get("/air-pump", (req,res) => airPump(req,res));
-router.get("/bicycle-repair-station", (req,res) => bicycleRepairStation(req,res));
-router.get("/bicyle-shop", (req,res) => bicycleShop(req,res));
-router.get("/bicycle-rental", (req,res) => bicycleRental(req,res));
+router.get("/air-pump", (req, res) => airPump(req, res));
+router.get("/bicycle-repair-station", (req, res) => bicycleRepairStation(req, res));
+router.get("/bicycle-shop", (req, res) => bicycleShop(req, res));
+router.get("/bicycle-rental", (req, res) => bicycleRental(req, res));
 
-
-router.get("/map-endpoints", (req,res)=>{})
-
+router.get("/endpoints", (req, res) => getMapEndpoints(req, res));
 
 async function bicycleParking(req, res) {
   res.status(501).json({ error: "not implemented" });
@@ -26,46 +24,48 @@ async function villoStation(req, res) {
 
   var villo_stations = [];
 
-  for (var i=0; i<json.data.length; i++) {
-      villo_stations[villo_stations.length] = {
-          name: json.data[i].name,
-          bike_stands: json.data[i].bike_stands,
-          lat: json.data[i].position.lat,
-          lng: json.data[i].position.lng,
-      };
+  for (var i = 0; i < json.data.length; i++) {
+    villo_stations[villo_stations.length] = {
+      name: json.data[i].name,
+      bike_stands: json.data[i].bike_stands,
+      lat: json.data[i].position.lat,
+      lng: json.data[i].position.lng,
+    };
   }
 
-  let data = await geojson.parse(villo_stations, {Point: ['lat', 'lng'], include: ['name', 'bike_stands']});
+  let data = await geojson.parse(villo_stations, {
+    Point: ["lat", "lng"],
+    include: ["name", "bike_stands"],
+  });
 
-  cache.add("testdata", data);
-  /*console.log(cache.get("testdata"));*/
+  cache.add("villo", data); //TODO DO SOMETHING WITH THIS CACHE
 
   return res.status(200).json(data);
 } //TODO switch conversion to convert.js
 
-async function airPump(req,res){
-    res.status(501).json({ error: "not implemented" });
+async function airPump(req, res) {
+  res.status(501).json({ error: "not implemented" });
 }
 
-async function bicycleRepairStation(req,res){
-    res.status(501).json({ error: "not implemented" });
+async function bicycleRepairStation(req, res) {
+  res.status(501).json({ error: "not implemented" });
 }
 
-async function bicycleShop(req,res){
-    res.status(501).json({ error: "not implemented" });
+async function bicycleShop(req, res) {
+  res.status(501).json({ error: "not implemented" });
 }
 
-async function bicycleRental(req,res){
-    res.status(501).json({ error: "not implemented" });
+async function bicycleRental(req, res) {
+  res.status(501).json({ error: "not implemented" });
 }
 
-
-
-
-
-
-function fetch(url) {
-  return axios.get(url);
+async function getMapEndpoints(req, res) {
+  let endpoints = router.stack.map((x) => "/api/v1/map" + x.route.path);
+  res
+    .status(200)
+    .json({ success: endpoints.filter((e) => e !== "/api/v1/map/endpoints") });
 }
+
+function fetch(url) {return axios.get(url)}
 
 module.exports = router;
