ee0282d2ecd215d7b1ad07d4b1688e6bc4f9f0b3
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-07-10 15:16:54 +0200

    switch conversion to conversion.js, optimize

--- api/v1/routes/map.js
+++ api/v1/routes/map.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const geojson = require("geojson");
 const cache = require("../scripts/cache");
+const Converter = require("../scripts/converter");
 
 router.get("/bicycle-parking", (req, res) => bicycleParking(req, res));
 router.get("/villo-stations", (req, res) => villoStation(req, res));
 router.get("/air-pump", (req, res) => airPump(req, res));
-router.get("/bicycle-repair-station", (req, res) => bicycleRepairStation(req, res));
+router.get("/bicycle-repair-station", (req, res) =>bicycleRepairStation(req, res));
 router.get("/bicycle-shop", (req, res) => bicycleShop(req, res));
 router.get("/bicycle-rental", (req, res) => bicycleRental(req, res));
 
@@ -18,33 +18,21 @@ async function bicycleParking(req, res) {
 }
 
 async function villoStation(req, res) {
-  if(cache.get("villo")){
+  if (cache.get("villo")) {
     return res.status(200).json(cache.get("villo"));
   }
 
-  let url = "https://api.jcdecaux.com/vls/v1/stations?apiKey=6d5071ed0d0b3b68462ad73df43fd9e5479b03d6&contract=Bruxelles-Capitale";
+  let url =
+    "https://api.jcdecaux.com/vls/v1/stations?apiKey=6d5071ed0d0b3b68462ad73df43fd9e5479b03d6&contract=Bruxelles-Capitale";
   let json = await fetch(url);
 
-  var villo_stations = [];
-
-  for (var i = 0; i < json.data.length; i++) {
-    villo_stations[villo_stations.length] = {
-      name: json.data[i].name,
-      bike_stands: json.data[i].bike_stands,
-      lat: json.data[i].position.lat,
-      lng: json.data[i].position.lng,
-    };
-  }
-
-  let data = await geojson.parse(villo_stations, {
-    Point: ["lat", "lng"],
-    include: ["name", "bike_stands"],
-  });
+  let converter = new Converter(json.data);
+  let data = converter.villoToGeoJson();
 
   cache.add("villo", data); //TODO add timeout?
 
   return res.status(200).json(data);
-} //TODO switch conversion to convert.js
+}
 
 async function airPump(req, res) {
   res.status(501).json({ error: "not implemented" });
@@ -69,6 +57,8 @@ async function getMapEndpoints(req, res) {
     .json({ success: endpoints.filter((e) => e !== "/api/v1/map/endpoints") });
 }
 
-function fetch(url) {return axios.get(url)}
+function fetch(url) {
+  return axios.get(url);
+}
 
 module.exports = router;
