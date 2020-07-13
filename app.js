a606fd7f2bd1cda964944a9b6c7919054c3ddb78
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-07-13 13:16:05 +0200

    add body parser

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 const apiRoutes = require("./routes/api");
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
