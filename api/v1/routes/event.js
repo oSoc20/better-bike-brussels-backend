cce6be66d0b095d444cc0414e28df2c2ad51abe9
Thomas Lavend'Homme <lavendthomas@outlook.be>
2020-07-15 15:57:28 +0200

    Fetch events from api.brussels

--- api/v1/routes/event.js
+++ api/v1/routes/event.js
@@ -1,8 +1,75 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
+const cache = require("../scripts/cache");
+const config = require("../../../config");
 
 router.get("/", (req,res) => {
     res.status(501).json({ error: "not implemented" });
 })
+
+router.get("/official", (req, res) => official_events(req, res));
+
+async function official_events(req, res) {
+    let key = "official_events"; // cache key
+
+    if (cache.get(key)) {
+        var data = cache.get(key);
+    } else {
+
+        const urls = [
+            "https://api.brussels:443/api/agenda/0.0.1/events/search?name=fiets",
+            "https://api.brussels:443/api/agenda/0.0.1/events/search?name=v%C3%A9lo",
+            "https://api.brussels:443/api/agenda/0.0.1/events/search?name=cycli"
+        ]
+
+        let jsons = urls.map(async function(item) {
+
+            try {
+                let json = await fetch(item);
+                return json.data;
+            } catch (err) {
+                console.log(err);
+                return null;
+            }
+        })
+
+        let jsons_nopromise = await Promise.all(jsons);
+
+        console.log(jsons_nopromise);
+
+        let all_events = {
+            events: []
+        };
+
+
+        jsons_nopromise.forEach((item) => {
+            if (item.response.resultCount > 0) {
+                item.response.results.event.forEach((elem) => {
+                    all_events.events.push(elem);
+                })
+            }
+        });
+
+        console.log(all_events);
+
+        res.status(200).json(all_events);
+    }
+}
+
+function fetch(url) {
+    const options = {
+        headers: {
+            'Authorization': getAPIKey(),
+            "Accept": "application/json"
+        }
+    }
+    return axios.get(url, options);
+}
+
+
+function getAPIKey() {
+    return config.env.APIBRUSSELS_API_KEY;
+}
 
 module.exports = router;
