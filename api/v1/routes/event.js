5363cfa2abe7432b3a808e9be978ee460ecdd251
Thomas Lavend'Homme <lavendthomas@outlook.be>
2020-07-15 16:59:52 +0200

    Add filtering to events

--- api/v1/routes/event.js
+++ api/v1/routes/event.js
@@ -4,14 +4,17 @@ const axios = require("axios");
 const cache = require("../scripts/cache");
 const config = require("../../../config");
 
-router.get("/", (req,res) => {
-    res.status(501).json({ error: "not implemented" });
+router.get("/", (req, res) => {
+    res.status(501).json({error: "not implemented"});
 })
 
 router.get("/official", (req, res) => official_events(req, res));
 
 async function official_events(req, res) {
     let key = "official_events"; // cache key
+
+    let from = parseInt(req.query.from);
+    let to = parseInt(req.query.to);
 
     if (cache.get(key)) {
         var data = cache.get(key);
@@ -23,7 +26,7 @@ async function official_events(req, res) {
             "https://api.brussels:443/api/agenda/0.0.1/events/search?name=cycli"
         ]
 
-        let jsons = urls.map(async function(item) {
+        let jsons = urls.map(async function (item) {
 
             try {
                 let json = await fetch(item);
@@ -35,8 +38,6 @@ async function official_events(req, res) {
         })
 
         let jsons_nopromise = await Promise.all(jsons);
-
-        console.log(jsons_nopromise);
 
         let all_events = {
             events: []
@@ -51,10 +52,30 @@ async function official_events(req, res) {
             }
         });
 
-        console.log(all_events);
+        all_events.events = filterByDate(all_events.events, from, to);
 
-        res.status(200).json(all_events);
+        return res.status(200).json(all_events);
     }
+}
+
+/**
+ * filter events depending on the date
+ * @param events an array of events
+ * @param from date in Unix Epoch time
+ * @param to date in Unix Epoch time
+ */
+function filterByDate(events, from, to) {
+    console.log(from + " " + to);
+    let filtered_events = events.filter((item) => {
+        // if event.data_next is between from and to, then include it
+        if (item.date_next) {
+            let next_date = new Date(item.date_next);
+            let next_time = next_date.getTime()/1000;
+            return from < next_time && next_time < to;
+        }
+        return false;
+    });
+    return filtered_events;
 }
 
 function fetch(url) {
