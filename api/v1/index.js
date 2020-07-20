ddd0c1cb8552f72c06da31f134e1f4df99c966ff
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-07-20 15:50:13 +0200

    add weather

--- api/v1/index.js
+++ api/v1/index.js
@@ -3,8 +3,10 @@ const app = express();
 
 const mapRoutes = require("./routes/map");
 const eventRoutes = require("./routes/event");
+const weatherRoutes = require("./routes/weather");
 
 app.use("/map", mapRoutes);
 app.use("/event", eventRoutes);
+app.use("/weather", weatherRoutes);
 
 module.exports = app;
