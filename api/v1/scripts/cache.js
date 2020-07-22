88253e53909d2fd705a173c4772bf10c66faa6b0
Thomas Lavend'Homme <lavendthomas@outlook.be>
2020-07-22 09:15:43 +0200

    Finish cache autorefresh

--- api/v1/scripts/cache.js
+++ api/v1/scripts/cache.js
@@ -1,24 +1,19 @@
 const cache = require("memory-cache");
 
-const dataFetcher = require("../scripts/datafetcher")
+const dataFetcher = require("../scripts/datafetcher");
+
+const OPENSTREETMAP_REFRESH = 86400000; // one day
+const VILLO_STATION_REFRESH = 3600000; // one hour
 
 module.exports = {
 
     init: async function() {
-        await cache.put("bicycle_parking", await dataFetcher.fetchBicycleParking(), 10, () => {
-        });
-        await cache.put("villo_station", await dataFetcher.fetchVilloStations(), 10, () => {
-        });
-        await cache.put("compressed_air", await dataFetcher.fetchAirPumps(), 100, () => {
-            console.log()
-        });
-        await cache.put("bicycle_repair_station", await dataFetcher.fetchBicycleRepairStation(), 1000, () => {
-        });
-        await cache.put("bicycle_shop", await dataFetcher.fetchBicycleShop(), 1000, () => {
-        });
-        await cache.put("drinking_water", await dataFetcher.fetchDrinkingWater(), 1000, () => {
-        });
-
+        await refreshBicycleParking();
+        await refreshVilloStation();
+        await refreshCompressedAir();
+        await refreshBicycleRepairStations();
+        await refreshBicycleShop();
+        await refreshDrinkingWater();
     },
     add: function (key, data) {
         cache.put(key, data);
@@ -27,4 +22,28 @@ module.exports = {
     get: function (key){
         return cache.get(key);
     }
+}
+
+async function refreshBicycleParking() {
+    await cache.put("bicycle_parking", await dataFetcher.fetchBicycleParking(), OPENSTREETMAP_REFRESH, refreshBicycleParking);
+}
+
+async function refreshVilloStation() {
+    await cache.put("villo_station", await dataFetcher.fetchVilloStations(), VILLO_STATION_REFRESH, refreshVilloStation);
+}
+
+async function refreshCompressedAir() {
+    await cache.put("compressed_air", await dataFetcher.fetchAirPumps(), OPENSTREETMAP_REFRESH, refreshCompressedAir);
+}
+
+async function refreshBicycleRepairStations() {
+    await cache.put("bicycle_repair_station", await dataFetcher.fetchBicycleRepairStation(), OPENSTREETMAP_REFRESH, refreshBicycleRepairStations);
+}
+
+async function refreshBicycleShop() {
+    await cache.put("bicycle_shop", await dataFetcher.fetchBicycleShop(), OPENSTREETMAP_REFRESH, refreshBicycleShop);
+}
+
+async function refreshDrinkingWater() {
+    await cache.put("drinking_water", await dataFetcher.fetchDrinkingWater(), OPENSTREETMAP_REFRESH, refreshDrinkingWater);
 }
